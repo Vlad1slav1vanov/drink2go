fe1ccad8e31ade986a939c59f66c5cc963027c70
Vladislav <typlomi@gmail.com>
2023-02-13 17:42:58 +0600

    completed footer

---
+++ source/js/leaflet.js
@@ -0,0 +1,23 @@
+const position = [59.968266, 30.3174089];
+const map = L.map('map').setView(position, 17);
+L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=HMfyPUotkQhoWYIXN1mz',{
+  crossOrigin: true
+}).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: '../img/icons/stack.svg#map-dot',
+  iconSize: [38, 50],
+  iconAnchor: [18, 50],
+});
+
+const mainPin = L.marker(
+  {
+    lat: position[0],
+    lng: position[1],
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+mainPin.addTo(map);
