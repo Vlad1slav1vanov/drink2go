bfa943f6e8ebd97673167817d2c0dff30810534d
Vladislav <typlomi@gmail.com>
2023-02-14 05:13:38 +0600

    tablet-layout slider + adventeges

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -2,7 +2,7 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.brow
 
 const swiper = new Swiper('.swiper', {
   loop: true,
-
+  
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
